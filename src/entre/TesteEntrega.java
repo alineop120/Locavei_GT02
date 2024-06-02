f23ad9fa82a7ee3fd475f05523de204c31b6c7c8
alineop120 <alineveira2@gmail.com>
2024-06-02 19:24:16 -0300

    modificando 19h24

--- src/entre/TesteEntrega.java
+++ src/entre/TesteEntrega.java
@@ -14,50 +14,50 @@ public class TesteEntrega
 {
     public static void main(String[] args) 
     {
-       Scanner sc = new Scanner(System.in);
-        boolean loop = true;
+        try (Scanner sc = new Scanner(System.in)) {
+            boolean loop = true;
             
-        while (loop) 
-        {
-            System.out.println("\nMENU ->");
-            System.out.println("1]Entrega Regular");
-            System.out.println("2]Entrega Atrasada");
-            System.out.println("3]Entrega com Danos");
-            System.out.println("4]ENCERRAR");
-            System.out.print("Opção: ");
-            int op = sc.nextInt();
-                
-            switch (op) 
+            while (loop) 
             {
-                case 1:
-                    System.out.println("Entre com os dados da Entrega Regular:");
-                    EntregaRegular rg = new EntregaRegular("", 0, null);
-                    rg.cadastra();
-                    System.out.println("-------------------------------------------------------");
-                    rg.mostra();
-                    break;
-                case 2:
-                    System.out.println("Entre com os dados da Entrega Atrasada:");
-                    EntregaAtrasada atr = new EntregaAtrasada("", 0, null, 0);
-                    atr.cadastra();
-                    System.out.println("-------------------------------------------------------");
-                    atr.mostra();
-                    break;
-                 case 3:
-                    System.out.println("Entre com os dados da Entrega com Danos:");
-                    EntregaComDanos entregaComDanos = new EntregaComDanos("", 0, null);
-                    entregaComDanos.cadastra();
-                    System.out.println("-------------------------------------------------------");
-                    entregaComDanos.mostra();
-                case 4:
-                    System.out.println("-------------------------------------------------------");
-                    System.out.println("\nPrograma encerrado!");
-                    loop = false;
-                    break;
-                default:
-                    System.out.println("\nOpção inválida! Por favor, escolha uma opção válida.");
+                System.out.println("\nMENU ->");
+                System.out.println("1]Entrega Regular");
+                System.out.println("2]Entrega Atrasada");
+                System.out.println("3]Entrega com Danos");
+                System.out.println("4]ENCERRAR");
+                System.out.print("Opção: ");
+                int op = sc.nextInt();
+                
+                switch (op)
+                {
+                    case 1:
+                        System.out.println("Entre com os dados da Entrega Regular:");
+                        EntregaRegular rg = new EntregaRegular("", 0, null);
+                        rg.cadastra();
+                        System.out.println("-------------------------------------------------------");
+                        rg.mostra();
+                        break;
+                    case 2:
+                        System.out.println("Entre com os dados da Entrega Atrasada:");
+                        EntregaAtrasada atr = new EntregaAtrasada("", 0, null, 0);
+                        atr.cadastra();
+                        System.out.println("-------------------------------------------------------");
+                        atr.mostra();
+                        break;
+                    case 3:
+                        System.out.println("Entre com os dados da Entrega com Danos:");
+                        EntregaComDanos entregaComDanos = new EntregaComDanos("", 0, null);
+                        entregaComDanos.cadastra();
+                        System.out.println("-------------------------------------------------------");
+                        entregaComDanos.mostra();
+                    case 4:
+                        System.out.println("-------------------------------------------------------");
+                        System.out.println("\nPrograma encerrado!");
+                        loop = false;
+                        break;
+                    default:
+                        System.out.println("\nOpção inválida! Por favor, escolha uma opção válida.");
+                }
             }
         }
-        sc.close();
     }
 }
